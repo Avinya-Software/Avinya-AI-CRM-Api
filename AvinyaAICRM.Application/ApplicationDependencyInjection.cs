aa878c82e48408fe0cc4c927091c99e110d9bb8a
Pintu Prasad <pintu.avinya@gmail.com>
2026-01-22 18:30:19 +0530

    Added Application

---
+++ AvinyaAICRM.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using AvinyaAICRM.Application.Interfaces.RepositoryInterface;
+using AvinyaAICRM.Application.Interfaces.RepositoryInterface.Tenant;
+using AvinyaAICRM.Application.Interfaces.RepositoryInterface.User;
+using AvinyaAICRM.Application.Interfaces.ServiceInterface;
+using AvinyaAICRM.Application.Interfaces.ServiceInterface.Auth;
+using AvinyaAICRM.Application.Interfaces.ServiceInterface.SuperAdmin;
+using AvinyaAICRM.Application.Services.Auth;
+using AvinyaAICRM.Application.Services.ErrorLog;
+using AvinyaAICRM.Infrastructure.Identity;
+using AvinyaAICRM.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvinyaAICRM.Application
+{
+    public static class ApplicationDependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddScoped<IErrorLogService, ErrorLogService>();
+            services.AddScoped<ISuperAdminService, SuperAdminService>();
+            return services;
+        }
+    }
+}
